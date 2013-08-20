7516c5b0997009b4bca2890b1d4e4c5ff5c787b4
Jared Wolinsky <jared.wolinsky@gmail.com>
2013-08-20 10:55:58 -0400

    commit deleted files

--- src/com/jarwol/wheredweeat/ui/ConfirmDialogListener.java
+++
@@ -1,9 +0,0 @@
-package com.jarwol.wheredweeat.ui;
-
-import android.app.DialogFragment;
-
-public interface ConfirmDialogListener {
-	public void onDialogPositiveClick(DialogFragment dialog);
-
-	public void onDialogNegativeClick(DialogFragment dialog);
-}
